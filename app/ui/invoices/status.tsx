ee79e9d0793e83f34adc91e6ce1c8e22275acee4
Reno Satya Adrian <renos.adrian@gmail.com>
2025-03-17 12:11:34 +0700

    css styling

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
+    // using clsx
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
