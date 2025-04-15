a42ce58785e42f3a3e36614cf0083dd3ff395e85
Reno Satya Adrian <renos.adrian@gmail.com>
2025-04-15 16:25:53 +0700

    add customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-    return <p>Customer Page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+      query?: string;
+    }>;
+  }) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+    
+    return (
+    <div className="w-full">
+      <Suspense>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
